499209611e035098b9fe92eb7c5429c243e80948
cikareto <nt.salisa@gmail.com>
2021-01-02 21:17:42 +0700

    copy hiding files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,10 @@
 const Generator = require("yeoman-generator");
-const chalk = require('chalk')
-const yosay = require('yosay')
-const mkdirp = require('mkdirp')
-const path = require('path')
+const chalk = require("chalk");
+const yosay = require("yosay");
+const mkdirp = require("mkdirp");
+const path = require("path");
 
-const prompts = require('./fixtures/prompts')
+const prompts = require("./fixtures/prompts");
 
 module.exports = class extends Generator {
   initializing() {
@@ -13,15 +13,11 @@ module.exports = class extends Generator {
 
   prompting() {
     this.log(
-      yosay(
-        `Welcome to the sensational ${chalk.cyan(
-          'cikareto'
-        )} generator!`
-      )
+      yosay(`Welcome to the sensational ${chalk.cyan("cikareto")} generator!`)
     );
 
     const appName = path.basename(process.cwd());
-    return this.prompt(prompts(appName)).then(props => {
+    return this.prompt(prompts(appName)).then((props) => {
       this.props = props;
     });
   }
@@ -29,24 +25,28 @@ module.exports = class extends Generator {
   default() {
     if (path.basename(this.destinationPath()) !== this.props.name) {
       this.log(
-        `Your project must be inside a folder named ${
-          chalk.bold.cyan(`'${this.props.name}'`)
-        }\nI'll automatically create this folder.\n`
+        `Your project must be inside a folder named ${chalk.bold.cyan(
+          `'${this.props.name}'`
+        )}\nI'll automatically create this folder.\n`
       );
       mkdirp(this.props.name);
       this.destinationRoot(this.destinationPath(this.props.name));
 
-      this.spawnCommandSync('git', ['init']);
+      this.spawnCommandSync("git", ["init"]);
     }
   }
 
   writing() {
-    // this.log(this.props.framework)
-    // const framework = this.props.framework
     this.fs.copy(
       this.templatePath(this.props.framework),
-      this.destinationPath('.')
-    )
+      this.destinationPath(".")
+    );
+
+    // copy hiding files
+    this.fs.copy(
+      this.templatePath(`${this.props.framework}/.*`),
+      this.destinationPath(".")
+    );
   }
 
   install() {
